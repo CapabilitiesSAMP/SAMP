fd02783d94ae2131c48026ac4efeb6cdef200991
CapabilitiesSAMP <42144343+CapabilitiesSAMP@users.noreply.github.com>
2018-08-06 17:07:02 +0200

    Update podstawa.js

--- podstawa.js
+++ podstawa.js
@@ -148,7 +148,7 @@ bot.on("message", function(message){
 	// Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
 	message.delete().catch(O_o=>{}); 
 	// And we get the bot to say the thing: 
-	message.channel.send(sayMessage);
+	msg.channel.send(sayMessage);
 	}
 
 	
