212fd6bf7956786130610ae3adb15baf32d52177
CapabilitiesSAMP <42144343+CapabilitiesSAMP@users.noreply.github.com>
2018-08-07 17:34:17 +0200

    Update podstawa.js

--- podstawa.js
+++ podstawa.js
@@ -123,26 +123,6 @@ bot.on("message", function(message){
 		}
 	}
 	
-	 if(c("purge")){
-	 {
-		let messagecount = parseInt(args[1]) || 1;
-
-		var deletedMessages = -1;
-
-		message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
-		    messages.forEach(m => {
-			if (m.author.id == bot.user.id) {
-			    m.delete().catch(console.error);
-			    deletedMessages++;
-			}
-		    });
-		}).then(() => {
-			if (deletedMessages === -1) deletedMessages = 0;
-			message.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
-			    .then(m => m.delete(2000));
-		}).catch(console.error);
-	 }
-	
 	if(c("avatar")){
 		if(input){
 			var a = findUserByName(input);
